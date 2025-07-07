6c2d38e307d728c6acd68107762aee7270ce6d58
Bright Munemu <brightmunemu@gmail.com>
2025-07-07 15:07:36 +0100

    refactor

--- src/components/(web-site)/_shared/header/index.tsx
+++ src/components/(web-site)/_shared/header/index.tsx
@@ -10,11 +10,11 @@ export default function Header() {
   const pathName = usePathname();
 
   return (
-    <header className="py-5 layout-spacing z-20 sticky bg-white top-0 left-0 w-full">
+    <header className="py-5 layout-spacing z-20 sticky bg-blackgray top-0 left-0 w-full">
       <div className="container mx-auto grid grid-flow-col justify-between lg:grid-cols-[auto_1fr_auto] items-center gap-8 lg:gap-16">
         {/* Logo */}
-        <p className="w-32 mx-auto">
-          Home<span className="text-primary">lands</span>
+        <p className="text-3xl font-bold">
+          Home<span className="text-[#003DA6]">lands</span>
         </p>
         {/* <LogoImage variant="black" className="w-32 mx-auto" /> */}
 
@@ -28,6 +28,14 @@ export default function Header() {
               variant={ButtonVariants.BlackOutlinedRounded}
             >
               Sign in
+            </Button>
+          </Link>
+          <Link href={"/login"} className="grid">
+            <Button
+              className="whitespace-nowrap border border-[#141414] rounded-xl"
+              variant={ButtonVariants.BlackOutlinedRounded}
+            >
+              Sign Out
             </Button>
           </Link>
         </div>
