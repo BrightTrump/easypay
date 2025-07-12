2135f5e4d64df9c64f6f319e5ad375967f8b48f1
Bright Munemu <brightmunemu@gmail.com>
2025-07-12 01:56:48 +0100

    create database

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,9 @@
+import { useToast } from "@/ui/toast/toast-utils";
 import mongoose from "mongoose";
+
 const MONGODB_URI = process.env.MONGODB_URI;
+
+const { callToast } = useToast();
 
 const connect = async () => {
   const connectionState = await mongoose.connection.readyState;
@@ -20,6 +24,12 @@ const connect = async () => {
     console.log("Connected");
   } catch (error: any) {
     console.log("Error: ", error);
+    callToast({
+      message: "An Unexpected error occured",
+      title: "Error",
+      type: "error",
+    });
     throw new Error("Error: ", error);
   }
 };
+export default connect;
