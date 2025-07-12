c35be7c3fe4f9f750dd0c029d3683f73e8f31e79
Bright Munemu <brightmunemu@gmail.com>
2025-07-12 11:55:56 +0100

    add files

--- src/components/web-site/_shared/header/navigation/navigation.tsx
+++ src/components/web-site/_shared/header/navigation/navigation.tsx
@@ -1,21 +1,70 @@
-"use client";
-
-import React from "react";
+import { Icon, Icons } from "@/ui";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import { useState } from "react";
+import Logo from "../../logo/Logo";
 import NavigationItem from "./navigation-item";
 
 export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const variants: Variants = {
+    initial: () => ({
+      height: 0,
+      opacity: 0,
+    }),
+    animate: {
+      height: "100%",
+      opacity: 1,
+    },
+  };
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      {/* <MobileNavigation /> */}
+      <button type="button">
+        <Icon type={Icons.Hamburger} size={32} color="#141414" />
+      </button>
+      {isOpen && (
+        <AnimatePresence>
+          <motion.div
+            variants={variants}
+            initial="initial"
+            animate="animate"
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            className="fixed inset-0 bg-white w-full h-[100svh] grid py-5 overflow-hidden"
+          >
+            <div className="grid gap-16 content-start overflow-hidden">
+              {/* Logo and Close button */}
+              <div className="px-5">
+                <div className="container mx-auto grid grid-flow-col items-center justify-between">
+                  <Logo />
 
-      <div className="hidden lg:grid grid-flow-col gap-5 w-max items-center hover:text-[#285ab1]">
-        <NavigationItem url="/">Home</NavigationItem>
-        <NavigationItem url="/about-us">About Us</NavigationItem>
-        <NavigationItem url="#services">Services</NavigationItem>
-        <NavigationItem url="#card">Card</NavigationItem>
-        <NavigationItem url="contact-us">Contact Us</NavigationItem>
-        <NavigationItem url="/ways-to-bank">Ways to Bank</NavigationItem>
-      </div>
+                  <button onClick={handleToggle}>
+                    <Icon type={Icons.Close} size={32} color="#141414" />
+                  </button>
+                </div>
+              </div>
+
+              {/* Navigations */}
+              <div className="overflow-y-auto custom-scroll-bar px-5">
+                <div className="hidden lg:grid grid-flow-col gap-5 w-max items-center hover:text-[#285ab1]">
+                  <NavigationItem url="/">Home</NavigationItem>
+                  <NavigationItem url="/about-us">About Us</NavigationItem>
+                  <NavigationItem url="#services">Services</NavigationItem>
+                  <NavigationItem url="#card">Card</NavigationItem>
+                  <NavigationItem url="contact-us">Contact Us</NavigationItem>
+                  <NavigationItem url="/ways-to-bank">
+                    Ways to Bank
+                  </NavigationItem>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      )}
     </>
   );
 }
