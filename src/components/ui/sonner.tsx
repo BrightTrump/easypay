0e728fdd1b0bd6a169eb5ac9092f45d82a0f3460
Bright Munemu <brightmunemu@gmail.com>
2025-07-12 02:11:54 +0100

    add line

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,25 +1,52 @@
-"use client"
+// "use client"
 
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+// import { useTheme } from "next-themes"
+// import { Toaster as Sonner, ToasterProps } from "sonner"
+
+// const Toaster = ({ ...props }: ToasterProps) => {
+//   const { theme = "system" } = useTheme()
+
+//   return (
+//     <Sonner
+//       theme={theme as ToasterProps["theme"]}
+//       className="toaster group"
+//       style={
+//         {
+//           "--normal-bg": "var(--popover)",
+//           "--normal-text": "var(--popover-foreground)",
+//           "--normal-border": "var(--border)",
+//         } as React.CSSProperties
+//       }
+//       {...props}
+//     />
+//   )
+// }
+
+// export { Toaster }
+
+"use client";
+
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
+      position="top-center"
+      toastOptions={{
+        style: {
+          paddingTop: "8px",
+          paddingBottom: "8px",
+        },
+        duration: 3000,
+        closeButton: true,
+      }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,10 @@
-import { useToast } from "@/ui/toast/toast-utils";
+// import { useToast } from "@/ui/toast/toast-utils";
 import mongoose from "mongoose";
+import { toast } from "sonner";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const { callToast } = useToast();
+// const { callToast } = useToast();
 
 const connect = async () => {
   const connectionState = await mongoose.connection.readyState;
@@ -24,11 +25,12 @@ const connect = async () => {
     console.log("Connected");
   } catch (error: any) {
     console.log("Error: ", error);
-    callToast({
-      message: "An Unexpected error occured",
-      title: "Error",
-      type: "error",
-    });
+    // callToast({
+    //   message: "An Unexpected error occured",
+    //   title: "Error",
+    //   type: "error",
+    // });
+    toast.error("Database Error");
     throw new Error("Error: ", error);
   }
 };
