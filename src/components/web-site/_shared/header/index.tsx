a29449a6174ddd25ba97e1931b57888027b2b5ff
Bright Munemu <brightmunemu@gmail.com>
2025-11-08 01:56:40 +0100

    udate

--- src/components/web-site/_shared/header/index.tsx
+++ src/components/web-site/_shared/header/index.tsx
@@ -5,15 +5,17 @@ import Logo from "../logo/Logo";
 import MobileNavigation from "./navigation/mobile-navigation";
 export default function Header() {
   return (
-    <header className="px-6 md:px-20 grid justify-between grid-cols-[1fr_auto] md:gap-8 items-center py-5 sm:pb-12 layout-spacing z-20 sticky bg-white top-0 left-0 w-full">
-      <Logo />
+    <header className="px-6 md:px-20 py-5 sm:pb-12 layout-spacing sticky top-0 left-0 z-20 bg-white w-full">
+      <div className="container mx-auto grid justify-between grid-cols-[1fr_auto] md:gap-8 items-center">
+        <Logo />
 
-      <div className="block md:hidden">
-        <MobileNavigation />
-      </div>
+        <div className="block md:hidden">
+          <MobileNavigation />
+        </div>
 
-      <div className="hidden sm:block">
-        <Navigation />
+        <div className="hidden sm:block">
+          <Navigation />
+        </div>
       </div>
     </header>
   );
