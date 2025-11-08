605db6c2434d6899e40877ab231fbdd495ff77db
Bright Munemu <brightmunemu@gmail.com>
2025-11-08 02:00:41 +0100

    aaa

--- src/components/web-site/home-page/our-services/our-sevices.tsx
+++ src/components/web-site/home-page/our-services/our-sevices.tsx
@@ -13,34 +13,36 @@ export default function OurServices() {
       className="py-20 layout-spacing grid gap-10 sm:gap-20"
     >
       {/* Header */}
-      <div className="md:max-w-2xl mx-auto grid place-items-center gap-4 text-center">
-        <p className="text-[#555555]">Our Services</p>
-        <h3 className="font-bold text-3xl sm:text-3xl md:text-4xl leading-normal">
-          We Offer Best Financial Consultancy Services in Affordable Cost
-        </h3>
-      </div>
+      <div className="container mx-auto">
+        <div className="md:max-w-2xl mx-auto grid place-items-center gap-4 text-center">
+          <p className="text-[#555555]">Our Services</p>
+          <h3 className="font-bold text-3xl sm:text-3xl md:text-4xl leading-normal">
+            We Offer Best Financial Consultancy Services in Affordable Cost
+          </h3>
+        </div>
 
-      {/* Our Services Grid */}
-      <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-5 rounded-xl px-6 lg:px-20">
-        {OUR_SERVICES_SCHEMA.map((item, index) => (
-          <motion.div
-            key={index}
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 3, delay: index * 0.2 }}
-            viewport={{ once: true }}
-            className="grid place-items-center gap-5 p-4 bg-white [&>div]:grid [&>div]:gap-4 [&>div]:text-center "
-          >
-            <Icon type={item.icon} size={80} color="#285ab1" />
-            <div>
-              <h2 className="text-2xl font-semibold">{item.label}</h2>
-              <p>{item.description}</p>
-            </div>
-          </motion.div>
-        ))}
-      </div>
+        {/* Our Services Grid */}
+        <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-5 rounded-xl px-6 lg:px-20">
+          {OUR_SERVICES_SCHEMA.map((item, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 3, delay: index * 0.2 }}
+              viewport={{ once: true }}
+              className="grid place-items-center gap-5 p-4 bg-white [&>div]:grid [&>div]:gap-4 [&>div]:text-center "
+            >
+              <Icon type={item.icon} size={80} color="#285ab1" />
+              <div>
+                <h2 className="text-2xl font-semibold">{item.label}</h2>
+                <p>{item.description}</p>
+              </div>
+            </motion.div>
+          ))}
+        </div>
 
-      <BestServices />
+        <BestServices />
+      </div>
     </section>
   );
 }
